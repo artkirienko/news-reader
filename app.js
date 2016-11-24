c77bfc0325f32a53cbb9b9909d16248700534d29
Artemy Kirienko <art-kirienko@ya.ru>
2016-11-24 23:27:52 +0300

    VIM-like navigation :snail:

--- app.js
+++ app.js
@@ -8,18 +8,34 @@ var main = function() {
   });
 
   $(document).keypress(function(event) {
-    if(event.which === 111) {
+    if(event.which === 108) {
       $('.description').hide();
 
       $('.current').children('.description').show();
     }
 
-    else if(event.which === 110) {
+    else if(event.which === 104) {
+      $('.description').hide();
+    }
+
+    else if(event.which === 106) {
       var currentArticle = $('.current');
       var nextArticle = currentArticle.next();
 
-      currentArticle.removeClass('current');
-      nextArticle.addClass('current');
+      if (nextArticle.hasClass('article')) {
+        currentArticle.removeClass('current');
+        nextArticle.addClass('current');
+      }
+    }
+
+    else if(event.which === 107) {
+      var currentArticle = $('.current');
+      var nextArticle = currentArticle.prev();
+
+      if (nextArticle.hasClass('article')) {
+        currentArticle.removeClass('current');
+        nextArticle.addClass('current');
+      }
     }
   });
 }
